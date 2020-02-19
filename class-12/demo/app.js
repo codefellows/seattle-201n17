d5f0331c29a0d67f03707cc2e1157950c32f9a87
Roger Huba <rogerhuba@gmail.com>
2020-02-18 16:56:01 -0800

    update demo

--- class-12/demo/app.js
+++ class-12/demo/app.js
@@ -62,53 +62,6 @@ function handelClick(e) {
     imageGenerator();
 }
 
-// Pulled initial code from char.js website.
-// TODO: Need an array of star Wars movie names to replace label
-// TODO: Need to made a votes array to replace data
-function renderChart(){
-    var ctx = document.getElementById('my_canvas').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: chartNames,
-        datasets: [{
-            label: '# of Votes',
-            data: chartVotes,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-}
-
-function addChartData(){
-
-}
-
 function populateData(){
     new StarWarsMovies('img/starwars1.jpg', 'starwars1', 'starwars1');
     new StarWarsMovies('img/starwars2.jpg', 'starwars2', 'starwars2');
@@ -125,4 +78,3 @@ function populateData(){
 
     populateData();
     imageGenerator();
-    renderChart();
